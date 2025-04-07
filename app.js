37bdb9dedb2f982644f6019edf8ec1f2b35332b8
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-04-07 21:28:43 +0600

    add middleware

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 import express from 'express'
+import core from 'cors'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
 
 
 const app = express()
 
 
-// 
+// middleware
+app.use(core({
+    credentials : true,
+    origin : process.env.CLIENT_URL
+}))
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(cookieParser())
+app.use(morgan())
+
 
 export default app
