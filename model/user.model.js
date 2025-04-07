d671e5828f17b1f2d695c8b2bc4c219563bb4f12
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-04-07 21:42:10 +0600

    user model

--- model/user.model.js
+++ model/user.model.js
@@ -1,36 +1,49 @@
 import mongoose from "mongoose";
 
-
-const userSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        require : true
-    },
-    
-    email : {
-        type : String,
-        require : true
-    },
-    authId : {
-        type : String,
-        require : true,
-        unique : true
-    },
-    applyJob : [{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'Job'
-    }],
-    saveJob : [{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'Job'
-    }],
-    role : {
-        type : String,
-        enum : ['jobseeker' , 'recruiter']
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
     },
 
-})
+    email: {
+      type: String,
+      require: true,
+    },
+    authId: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    applyJob: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+      },
+    ],
+    saveJob: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+      },
+    ],
+    role: {
+      type: String,
+      enum: ["jobseeker", "recruiter"],
+    },
+    bio: {
+      type: String,
+      default: "No bio provide",
+    },
+    profession: {
+      type: String,
+      default: "Unemployed",
+    },
+  },
+  { timestamps: true }
+);
 
-const userModel = mongoose.model('User' , userSchema);
+const userModel = mongoose.model("User", userSchema);
 
 export default userModel;
