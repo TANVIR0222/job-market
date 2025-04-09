559b72190b2d4e9dc095f488ddabef48a4162bc3
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-04-09 22:13:26 +0600

    user Login controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,6 +1,7 @@
 import userModel from "../model/user.model.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
 // user register
 export const userRegister = async (req, res) => {
@@ -46,3 +47,36 @@ export const userRegister = async (req, res) => {
     res.status(500).json(new ApiResponse(500, error, "user create fail "));
   }
 };
+
+
+// user login
+
+export const userLogin = asyncHandler(async(req,res)=>{
+  
+    const {email , password} = req.body;
+    console.log(email , password);
+    
+
+    if(!email){
+        throw new ApiError(500, "Please  provide email ");
+    }
+
+    const user = await userModel.findOne({email})
+    if(!user){
+        res.status(500).json(new ApiResponse(500, error, "user not found , login now "));
+    }
+    
+
+    // match user send password === data base password  
+    const matchPassword =  await user.isPasswordCorrect(password) // check password  // userModel isPasswordCorrect
+    if (!matchPassword) {
+        res.status(401).json(new ApiResponse(401 , {} , "Password not match"));
+    }
+
+
+    const loginUserData = await userModel.findById(user._id);
+    res
+    .status(201)
+    .json(new ApiResponse(201, loginUserData, "user create success "));
+
+})
